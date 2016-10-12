beb095173703e54993061dd77af57ff97cbfe8e5
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-10-11 20:58:40 -0500

    Set up initial game controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,9 +12,24 @@ class GameController < ApplicationController
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = ["rock", "paper", "scissors"].sample
 
-    @result = "Replace this string with the correct value."
+    if @user_move == @computer_move
+      @result  = "tie"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @result  = "lose"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @result  = "win"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @result  = "win"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @result  = "lose"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @result  = "lose"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @result  = "win"
+    end
+    
 
     # ===============================================================
     # Your code goes above.
